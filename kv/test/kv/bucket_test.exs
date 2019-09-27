dd320790218693f872694947675d12aff4456664
jgriffin <jeremy.griffin@movehq.com>
2019-09-27 14:05:58 -0400

    test: Add agent bucket module test.
    We will use an agent bucket module to pass messages around it seems.  So
    we need a test to make sure it will function.
    
    On branch master
        New file:   test/kv/bucket_test.exs

---
+++ kv/test/kv/bucket_test.exs
@@ -0,0 +1,12 @@
+defmodule KV.BucketTest do
+  ## @NOTE: async: true should be omitted if there is any IO access that may be synchronous
+  use ExUnit.Case, async: true
+
+  test "stores values by key" do
+    {:ok, bucket} = KV.Bucket.start_link([])
+    assert KV.Bucket.get(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == 3
+  end
+end
