e93ef05e17c9d8680f9abe718f0ed6b6148d5c3a
jgriffin <jeremy.griffin@movehq.com>
2019-09-27 14:05:58 -0400

    test: Bucket delete function test added.
    Add a test for deleting a key from a bucket.

--- kv/test/kv/bucket_test.exs
+++ kv/test/kv/bucket_test.exs
@@ -13,4 +13,14 @@ defmodule KV.BucketTest do
     KV.Bucket.put(bucket, "milk", 3)
     assert KV.Bucket.get(bucket, "milk") == 3
   end
+
+  test "delete values by key", %{bucket: bucket} do
+    assert KV.Bucket.get(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == 3
+
+    KV.Bucket.delete(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == nil
+  end
 end
